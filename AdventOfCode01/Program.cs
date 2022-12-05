211b5b60dfb84819bec867b39b6c8e5b5834bc07
bialasikdev <p110163436@student.stamford.ac.uk>
2022-12-05 21:59:34 +0000

    [1] AOC22
    Days 1 and 2.

---
+++ AdventOfCode01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.WebRequestMethods;
+
+namespace AdventOfCode01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"";
+            string line = String.Empty;
+
+            StreamReader reader;
+
+            int x = 0;
+            List<int> elfCalories = new List<int>();
+
+            Console.WriteLine("----------------------------" +
+                "\n   Advent of Code: Day 01" +
+                "\n----------------------------");
+
+            reader = new StreamReader(path);
+            while (!reader.EndOfStream)
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    if (String.IsNullOrEmpty(line))
+                    {
+                        elfCalories.Add(x);
+                        x = 0;
+                    }
+                    else
+                    {
+                        x += Convert.ToInt32(line.ToString());
+                    }
+                }
+            }
+            //Console.WriteLine("Total Elf Calories: ");
+            //elfCalories.ForEach(Console.WriteLine);
+
+            Console.WriteLine($"Top 3 Calories" +
+                $"\n1: {elfCalories.Max()}\n");
+
+            x = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                x += elfCalories.Max();
+                elfCalories.RemoveAt(elfCalories.IndexOf(elfCalories.Max()));
+            }
+            Console.WriteLine($"Top 3 Elf Calorie: {x}");
+        }
+    }
+}
---
+++ AdventOfCode02/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode02
+{
+    internal class Program
+    {
+        // rock += 1        |   win += 6
+        // paper += 2       |   draw += 3
+        // scissors += 3    |   loss += 0
+
+        // A = rock(r), B = paper(p), C = scissors(s)
+        // X = rock(r), Y = paper(p), Z = scissors(s)
+
+        static void Main(string[] args)
+        {
+            string path = @"";
+            StreamReader reader;
+
+            List<string> matches = new List<string>();
+
+            int score = 0;
+            string line = String.Empty;
+
+            Console.WriteLine("----------------------------" +
+                "\n   Advent of Code: Day 02" +
+                "\n----------------------------");
+
+            reader = new StreamReader(path);
+            while ((line = reader.ReadLine()) != null)
+            {
+                switch (line[0])
+                {
+                    case 'A':
+                        switch (line[2])
+                        {
+                            case 'X':
+                                score += 3;
+                                break;
+                            case 'Y':
+                                score += 4;
+                                break;
+                            case 'Z':
+                                score += 8;
+                                break;
+                        }
+                        break;
+                    case 'B':
+                        switch (line[2])
+                        {
+                            case 'X':
+                                score += 1;
+                                break;
+                            case 'Y':
+                                score += 5;
+                                break;
+                            case 'Z':
+                                score += 9;
+                                break;
+                        }
+                        break;
+                    case 'C':
+                        switch (line[2])
+                        {
+                            case 'X':
+                                score += 2;
+                                break;
+                            case 'Y':
+                                score += 6;
+                                break;
+                            case 'Z':
+                                score += 7;
+                                break;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine($"Score: {score}");
+        }
+    }
+}
